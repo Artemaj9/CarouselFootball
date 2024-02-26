beb8bfc74c1bfe7b2dc31e8d0aa5beb824dc3a4d
ArtemMoiseev <moiseev@physics.msu.ru>
2024-02-27 00:11:56 +0300

    Initial Commit

---
+++ CarouselFootball/CarouselFootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselFootballApp.swift
+//  CarouselFootball
+//
+//  Created by Artem on 27.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct CarouselFootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
